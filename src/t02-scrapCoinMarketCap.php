6dee115fcee66a018e68dd61e4953cb86424f07a
marcpre <marcus.presich@gmail.com>
2017-12-07 06:36:52 +0000

    adding print to json!

--- src/t02-scrapCoinMarketCap.php
+++ src/t02-scrapCoinMarketCap.php
@@ -10,24 +10,34 @@ $url = 'td.no-wrap.currency-name > a';
 $symbol = 'td.text-left.col-symbol';
 $price = 'td:nth-child(5) > a';
 
+$result = array();
+
 $crawler = $client->request('GET', 'https://coinmarketcap.com/all/views/all/');
 
-$crawler->filter($coin)->each(function ($node) {
+$crawler->filter($coin)->each(function ($node) use (&$result) {
     print $node->text()."\n";
+    array_push($result, $node->text());
 });
 
-$crawler->filter($url)->each(function ($node) {
+$crawler->filter($url)->each(function ($node) use (&$result) {
     $link = $node->link();
     $uri = $link->getUri();
     print $uri."\n";
+    array_push($result, $uri);
 });
 
-
-$crawler->filter($symbol)->each(function ($node) {
+$crawler->filter($symbol)->each(function ($node) use (&$result) {
     print $node->text()."\n";
+    array_push($result, $node->text());
 });
 
-$crawler->filter($price)->each(function ($node) {
+$crawler->filter($price)->each(function ($node) use (&$result) {
     print $node->text()."\n";
+    array_push($result, $node->text());
 });
 
+print_r($result);
+
+$json_data = json_encode($result);
+file_put_contents('data/myfile.json', $json_data);    
+    

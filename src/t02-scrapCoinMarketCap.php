cc4854f6276ee847fdc3c7252309e341f1a60704
marcpre <marcus.presich@gmail.com>
2017-12-08 15:42:58 +0100

    creating json file

--- src/t02-scrapCoinMarketCap.php
+++ src/t02-scrapCoinMarketCap.php
@@ -10,34 +10,41 @@ $url = 'td.no-wrap.currency-name > a';
 $symbol = 'td.text-left.col-symbol';
 $price = 'td:nth-child(5) > a';
 
-$result = array();
+//arrays
+$coinArr = array();
+$urlArr = array();
+$symbolArr = array();
+$priceArr = array();
 
 $crawler = $client->request('GET', 'https://coinmarketcap.com/all/views/all/');
 
-$crawler->filter($coin)->each(function ($node) use (&$result) {
+$crawler->filter($coin)->each(function ($node) use (&$coinArr) {
     print $node->text()."\n";
-    array_push($result, $node->text());
+    array_push($coinArr, $node->text());
 });
 
-$crawler->filter($url)->each(function ($node) use (&$result) {
+$crawler->filter($url)->each(function ($node) use (&$urlArr) {
     $link = $node->link();
     $uri = $link->getUri();
     print $uri."\n";
-    array_push($result, $uri);
+    array_push($urlArr, $uri);
 });
 
-$crawler->filter($symbol)->each(function ($node) use (&$result) {
+$crawler->filter($symbol)->each(function ($node) use (&$symbolArr) {
     print $node->text()."\n";
-    array_push($result, $node->text());
+    array_push($symbolArr, $node->text());
 });
 
-$crawler->filter($price)->each(function ($node) use (&$result) {
+$crawler->filter($price)->each(function ($node) use (&$priceArr) {
     print $node->text()."\n";
-    array_push($result, $node->text());
+    array_push($priceArr, $node->text());
 });
 
-print_r($result);
-
-$json_data = json_encode($result);
+//Multi Dimensional Array
+$multi = array();
+foreach($coinArr as $key => $v) {
+    $multi[] = [$coinArr[$key], $urlArr[$key], $symbolArr[$key], $priceArr[$key]];
+}
+$json_data = json_encode($multi);
 file_put_contents('data/myfile.json', $json_data);    
     

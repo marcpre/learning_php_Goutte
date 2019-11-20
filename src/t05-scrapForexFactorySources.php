c9019d1d5f7988e9bbff3d82189b39d9366dea9a
marcpre <marcus.presich@gmail.com>
2019-11-20 13:10:57 +0100

    reworked scrap Forex Factory

--- src/t05-scrapForexFactorySources.php
+++ src/t05-scrapForexFactorySources.php
@@ -5,23 +5,22 @@ use Symfony\Component\DomCrawler\Crawler;
 
 
 $x = 1;
-$LIMIT = 10;
+$LIMIT = 20;
 
 $client = new Client();
 $crawler = $client->request('GET', 'https://www.forexfactory.com/calendar.php?month=nov.2019');
-
-
+$resArray = array();
+$TEMP = array();
 
 
 $crawler->filter('.calendar_row')->each(function ($node) {
     global $x;
     global $LIMIT;
+    global $resArray;
+    global $TEMP;
     $x++;
 
-
     $EVENTID   = $node->attr('data-eventid');
-    $EVENTNAME = $node->filter('.event')->first()->filter('div')->first()->text();
-    echo "<h4>".$EVENTNAME."</h4><br>";
 
     $API_RESPONSE = file_get_contents('https://www.forexfactory.com/flex.php?do=ajax&contentType=Content&flex=calendar_mainCal&details='.$EVENTID);
 
@@ -39,20 +38,31 @@ HTML;
 
     $subcrawler = new Crawler($html);
 
-
-
     $subcrawler->filter('.calendarspecs__spec')->each(function ($LEFT_TD) {
-
+        global $resArray;
+        global $TEMP;
         $LEFT_TD_INNER_TEXT = trim($LEFT_TD->text());
 
-        if($LEFT_TD_INNER_TEXT == "Source" || $LEFT_TD_INNER_TEXT == "Usual Effect"){
-            echo $LEFT_TD_INNER_TEXT.": ".$LEFT_TD->nextAll()->html()."<br>";
+        if($LEFT_TD_INNER_TEXT == "Source"){
+
+            $TEMP = array();
+            $LEFT_TD->nextAll()->filter('a')->each(function ($LINK) {
+                global $TEMP;
+                array_push($TEMP,$LINK->text(),$LINK->attr('href'));
+            });
+
+            $EVENT['sourceTEXT'] = $TEMP[0];
+            $EVENT['sourceURL']  = $TEMP[1];
+            $EVENT['latestURL']  = $TEMP[3];
+
+            array_push($resArray,$EVENT);
         }
 
     });
 
-    if($x>$LIMIT)
+    if($x>$LIMIT){
+        echo "<pre>"; var_dump($resArray); echo "</pre>";
         exit;
-    echo "<hr>";
+    }
 
 });

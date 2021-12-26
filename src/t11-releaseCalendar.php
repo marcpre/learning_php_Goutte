bf28d5c60b99aed82bd32e911b2963d279709387
marcpre <marcus.presich@gmail.com>
2021-12-26 11:26:57 +0100

    fixing release calendar

--- src/t11-releaseCalendar.php
+++ src/t11-releaseCalendar.php
@@ -5,6 +5,8 @@ require_once '../vendor/autoload.php';
 use Symfony\Component\DomCrawler\Crawler;
 use Goutte\Client;
 
+
+// fix crawler - https://stackoverflow.com/questions/70402267/goutte-get-list-with-date-on-top-and-title-below
 try {
 
     $resArr = array();
@@ -17,22 +19,20 @@ try {
     $content = $client->request('GET', $url)->html();
     $crawler = new Crawler($content, null, null);
 
-    $table = $crawler->filter('#schedule'); //->first()->closest('table');
+    $table = $crawler->filter('#schedule');
 
-    $index = 0;
-    $resArr = array();
+    // use today's date as a default, in case first one is missing
+    $releaseDate = (new DateTime())->format("m/d/y");
     $table->filter('div')
-        ->each(function (Crawler $tr) use (&$index, &$resArr) {
-
+        ->each(function (Crawler $tr) use (&$index, &$resArr, &$releaseDate) {
             if ($tr->filter('.schedule-date')->count() > 0) {
+                // update the date if it exists, otherwise continue with the old one
                 $releaseDate = $tr->filter('.schedule-date')->text();
             }
-
             if ($tr->filter('div > div.eight.columns > a')->count() > 0) {
                 $releaseStr = $tr->filter('div > div.eight.columns > a')->text();
-                array_push($resArr, [$releaseDate, $releaseStr]);
+                $resArr[] = [$releaseDate, $releaseStr];
             }
-
         });
 
     var_dump($resArr);
